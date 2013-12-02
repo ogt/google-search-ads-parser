ac8aa9c3efe12852182f12ee3c01a5e5d094d542
Pavel <pavel.evst@gmail.com>
2013-12-02 18:51:36 +0700

    single point of call

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 exports.fetcher = require('./lib/fetcher').fetcher;
 exports['static'] = require('./lib/static')['static'];
 exports.extended = require('./lib/extended').extended;
+
+exports.parseFile = function (file, options) {
+  if (!options) options = {};
+  if (options.expanded_html == undefined) options.expanded_html = true;
+
+  if (options.expanded_html) {
+    return exports.extended.parseFile(file);
+  } else {
+    return exports['static'].parseFile(file);
+  }
+};
