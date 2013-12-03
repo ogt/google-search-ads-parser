e9702602b1832aad8b18fa23f63e48223f733a80
Pavel <pavel.evst@gmail.com>
2013-12-03 11:43:33 +0700

    Cut to modules

--- test/extended.js
+++ test/extended.js
@@ -7,6 +7,11 @@ var parser = require("../index");
 describe('parser.extended', function () {
   this.timeout(15000);
 
+  it('should parse file searchable', function() {
+    var res = parser.extended.parseFile('test/rich_data/1.html');
+    assert.equal(res.query_string, 'indonesia flight ticket');
+  });
+
   it('should parse file', function() {
     var data = parser.extended.parseFile('test/rich_data/1.html');
     var first = data.ads[0];
